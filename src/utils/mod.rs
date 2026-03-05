87df01ccf409edcbc68d6a41889cfe88bc16c053
aiv <amishavhere@gmail.com>
2026-03-05 16:42:41 +0200

    updated HandleMonitor to actually drop correctly

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,7 @@
-use std::time::Duration;
+use std::{
+    sync::{Arc, atomic::AtomicBool},
+    time::Duration,
+};
 
 use tokio::{sync::Mutex, task::JoinHandle};
 
@@ -10,38 +13,47 @@ macro_rules! dispatch {
     };
 }
 
-pub struct HandleMonitor(pub Mutex<Vec<JoinHandle<()>>>);
+pub struct HandleMonitor {
+    handles: Mutex<Vec<JoinHandle<()>>>,
+    destroyed: AtomicBool,
+}
 
 impl HandleMonitor {
     pub fn new() -> Self {
-        let s = Self(Mutex::new(Vec::new()));
-        s
+        Self {
+            handles: Mutex::new(Vec::with_capacity(32)),
+            destroyed: AtomicBool::new(false),
+        }
     }
 
     pub async fn size(&self) -> usize {
-        let handles = self.0.lock().await;
+        let mut handles = self.handles.lock().await;
+        handles.retain(|h| !h.is_finished());
         handles.len()
     }
 
-    pub async fn init(&self) {
-        loop {
+    pub async fn init(self: Arc<Self>) {
+        while !self.destroyed.load(std::sync::atomic::Ordering::Relaxed) {
             tokio::time::sleep(Duration::from_millis(250)).await;
             self.prune().await;
         }
     }
 
     pub async fn add(&self, handle: JoinHandle<()>) {
-        let mut handles = self.0.lock().await;
+        let mut handles = self.handles.lock().await;
         handles.push(handle);
     }
 
     pub async fn prune(&self) {
-        let mut handles = self.0.lock().await;
+        let mut handles = self.handles.lock().await;
         handles.retain(|h| !h.is_finished());
     }
 
     pub async fn flush(&self) {
-        let mut handles = self.0.lock().await;
+        self.destroyed
+            .store(true, std::sync::atomic::Ordering::Relaxed);
+
+        let mut handles = self.handles.lock().await;
         for handle in handles.drain(..) {
             handle.await;
         }
