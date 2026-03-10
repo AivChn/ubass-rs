e4a9489371bd6df6b4aeb8c541cd489db2558048
aiv <amishavhere@gmail.com>
2026-03-10 21:13:00 +0200

    PROTOCOL_EPOCH definition

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,8 @@
+use std::sync::OnceLock;
+
 pub use crate::error::*;
 pub use crate::utils::*;
+use tokio::time::Instant;
 pub use ubass_macros::*;
+
+pub static PROTOCOL_EPOCH: OnceLock<Instant> = OnceLock::new();
