4ad4b121cf7e792dac5db0996ccf20ccce920bcb
aiv <amishavhere@gmail.com>
2026-03-12 15:24:25 +0200

    added documentation and renamed some stuff for clarity

--- src/transport/types.rs
+++ src/transport/types.rs
@@ -1,5 +1,6 @@
 use crate::{
     packet_processor::types::{PacketId, ProcessedPacket, TransportMessage},
+    packetizer::types::MAX_PAYLOAD_LENGTH,
     prelude::*,
 };
 use std::{cmp::min, fmt::Debug, sync::Arc};
@@ -13,22 +14,26 @@ pub const MAX_CONCURRENT_SENDS: usize = 128;
 pub const MAX_PACKET_BUFFER_SIZE: usize = 128;
 pub const BUFFER_TIMEOUT: u64 = 25;
 
+/// Packaging for the two channels the inbound task accesses
 pub struct InboundChannels {
     pub receiver: Receiver<TransportMessage>,
     pub sender: Sender<Result<ReceivedPacket>>,
 }
 
+/// A struct to manage outbound sockets, using a ring buffer esque structure that expands and
+/// contracts automatically based on throughput
 pub struct OutboundSockets {
     sockets: Vec<Arc<UdpSocket>>,
     current_socket: usize,
-    swappinness: u64,
-    removal: u64,
-    addition: u64,
+    swappinness_meter: u64,
+    removal_meter: u64,
+    addition_meter: u64,
     early_batches: u64,
     on_time_batches: u64,
 }
 
 impl OutboundSockets {
+    /// Customizeable thresholds
     const SWAPPINNESS_THRESHOLD: u64 = 5_760;
     const REMOVAL_THRESHOLD: u64 = 54_651;
     const ADDITION_THRESHOLD: u64 = 26_224;
@@ -48,14 +53,15 @@ impl OutboundSockets {
         Ok(Self {
             sockets,
             current_socket: 0,
-            swappinness: 0,
-            removal: 0,
-            addition: 0,
+            swappinness_meter: 0,
+            removal_meter: 0,
+            addition_meter: 0,
             early_batches: 0,
             on_time_batches: 0,
         })
     }
 
+    /// Returns the current socket as an Arc
     pub fn retrieve(&self) -> Arc<UdpSocket> {
         self.sockets[self.current_socket].clone()
     }
@@ -68,26 +74,33 @@ impl OutboundSockets {
     pub async fn update(&mut self, elapsed: u64) -> ErrResult {
         #[allow(clippy::cast_possible_truncation)]
         #[allow(clippy::cast_sign_loss)]
+        // get the time left until the buffer would have been sent, 0 if the buffer was sent at the
+        // last moment
         let n = min(BUFFER_TIMEOUT as i128 - elapsed as i128, 0) as u64;
         if n == 0 {
+            // if the buffer wasnt filled or filled just at the last second,
+            // reset early batches and increase on time batches.
             self.early_batches = 1;
             self.on_time_batches += 1;
         } else {
+            // vice versa
             self.on_time_batches = 1;
             self.early_batches += 1;
         }
 
-        self.swappinness += n * n;
-        self.removal += (n * n) - ((n * n) / self.on_time_batches);
-        self.addition += (n * n) - ((n * n) / self.early_batches);
+        // calculate the meters
+        self.swappinness_meter += n * n;
+        // 0 increase if the relevant batch counter just reset
+        self.removal_meter += (n * n) - ((n * n) / self.on_time_batches);
+        self.addition_meter += (n * n) - ((n * n) / self.early_batches);
 
-        if self.swappinness >= Self::SWAPPINNESS_THRESHOLD {
-            self.swappinness = 0;
+        if self.swappinness_meter >= Self::SWAPPINNESS_THRESHOLD {
+            self.swappinness_meter = 0;
             self.next_socket();
         }
 
-        if self.removal >= Self::REMOVAL_THRESHOLD {
-            self.removal = 0;
+        if self.removal_meter >= Self::REMOVAL_THRESHOLD {
+            self.removal_meter = 0;
             if self.current_socket == self.sockets.len() - 1 {
                 self.next_socket();
             }
@@ -95,14 +108,19 @@ impl OutboundSockets {
                 .remove((self.current_socket + 1) % self.sockets.len());
         }
 
-        if self.addition >= Self::ADDITION_THRESHOLD {
-            self.addition = 0;
+        if self.addition_meter >= Self::ADDITION_THRESHOLD {
+            self.addition_meter = 0;
             self.sockets.push(Self::new_socket().await?);
         }
 
         Ok(())
     }
 
+    /// Adds a socket to the buffer
+    ///
+    /// # Errors
+    ///
+    /// can return a `FailedToBind` error if socket binding failed
     async fn new_socket() -> Result<Arc<UdpSocket>> {
         Ok(Arc::new(
             UdpSocket::bind("0.0.0.0:0")
@@ -111,6 +129,7 @@ impl OutboundSockets {
         ))
     }
 
+    /// swaps the current socket to the next one
     fn next_socket(&mut self) {
         self.current_socket = (self.current_socket + 1) % self.sockets.len();
     }
@@ -124,6 +143,7 @@ pub struct SendablePacket {
     pub duplicate_count: usize,
 }
 
+/// A packet straight from the socket oven
 #[repr(transparent)]
 #[derive(Debug, Clone)]
 pub struct ReceivedPacket {
