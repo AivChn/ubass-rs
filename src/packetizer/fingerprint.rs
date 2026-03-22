4e1e25ad56fbd5cf8b43a281d23a5a464a504d85
aiv <amishavhere@gmail.com>
2026-03-22 21:26:19 +0200

    refactored Fingerprint to rely on HeaderSerialize

--- src/packetizer/fingerprint.rs
+++ src/packetizer/fingerprint.rs
@@ -1,3 +1,13 @@
-pub trait Fingerprint {
+pub trait HeaderSerialize {
+    fn headers(&self) -> Vec<u8>;
+}
+
+pub trait Fingerprint: HeaderSerialize {
     fn fingerprint(&self) -> [u8; 16];
+}
+
+impl<T: HeaderSerialize> Fingerprint for T {
+    fn fingerprint(&self) -> [u8; 16] {
+        xxhash_rust::xxh3::xxh3_128(&self.headers()).to_be_bytes()
+    }
 }
