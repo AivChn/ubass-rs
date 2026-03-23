4ee07dff5a74ee320e4d995c4b301b6894f4c5b8
aiv <amishavhere@gmail.com>
2026-03-23 15:48:33 +0200

    added FingerprintMonitor

--- src/manager/types.rs
+++ src/manager/types.rs
@@ -1,11 +1,21 @@
 use std::{
-    collections::HashMap,
-    sync::{Arc, atomic::AtomicU64},
+    collections::{HashMap, HashSet, VecDeque},
+    hash::Hash,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, AtomicU64, Ordering},
+    },
+    time::Duration,
 };
 
-use aes_gcm_siv::Aes256GcmSiv;
+use aes_gcm_siv::{Aes256GcmSiv, Nonce};
+use tokio::sync::Mutex;
+use uniffi::{ForeignBytes, foreignbytes};
 
-use crate::packetizer::types::SessionId;
+use crate::packetizer::{
+    fingerprint,
+    types::{PacketFingerprint, SessionId, Timestamp},
+};
 
 pub enum ManagerMessage {
     Close,
@@ -17,6 +27,105 @@ pub enum HighwayMessage {
     Packetizer,
     PacketProcessor,
     Transport,
+}
+
+#[derive(Eq)]
+struct FingerprintPtr(*const PacketFingerprint);
+
+unsafe impl Send for FingerprintPtr {}
+unsafe impl Sync for FingerprintPtr {}
+
+impl FingerprintPtr {
+    fn from_box(value: &Box<PacketFingerprint>) -> Self {
+        Self(&**value as *const PacketFingerprint)
+    }
+}
+
+impl PartialEq for FingerprintPtr {
+    fn eq(&self, other: &Self) -> bool {
+        unsafe { (*self.0) == (*other.0) }
+    }
+}
+
+impl Hash for FingerprintPtr {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        unsafe {
+            (*self.0).hash(state);
+        }
+    }
+}
+
+pub struct FingerprintMonitor {
+    fingerprints: Mutex<HashSet<Box<PacketFingerprint>>>,
+    queue: Mutex<VecDeque<(Timestamp, FingerprintPtr)>>,
+    canceled: AtomicBool,
+}
+
+impl Default for FingerprintMonitor {
+    fn default() -> Self {
+        Self {
+            fingerprints: Mutex::new(HashSet::new()),
+            queue: Mutex::new(VecDeque::new()),
+            canceled: AtomicBool::new(false),
+        }
+    }
+}
+
+impl FingerprintMonitor {
+    const PRUNE_INTERVAL: u64 = 7 * 1000;
+
+    pub async fn init(self: Arc<Self>) {
+        tokio::spawn(self.prune());
+    }
+
+    pub async fn add(&self, fingerprint: Box<PacketFingerprint>) -> bool {
+        let ptr = 'add_to_set: {
+            let mut fingerprints = self.fingerprints.lock().await;
+            let ptr = FingerprintPtr::from_box(&fingerprint);
+            if fingerprints.insert(fingerprint) {
+                Some(ptr)
+            } else {
+                None
+            }
+        };
+
+        let Some(ptr) = ptr else {
+            return false;
+        };
+
+        'add_to_queue: {
+            let mut queue = self.queue.lock().await;
+            queue.push_back((Timestamp::now(), ptr));
+        }
+
+        true
+    }
+
+    pub async fn prune(self: Arc<Self>) {
+        let mut expired = Vec::with_capacity(256);
+        while !self.canceled.load(Ordering::Relaxed) {
+            'pop_queue: {
+                let mut queue = self.queue.lock().await;
+                while queue
+                    .front()
+                    .is_some_and(|(ts, _)| ts.been_longer_than(Self::PRUNE_INTERVAL))
+                {
+                    if let Some((_, value)) = queue.pop_front() {
+                        expired.push(value);
+                    }
+                }
+            }
+
+            'remove: {
+                let mut fingerprints = self.fingerprints.lock().await;
+                expired
+                    .drain(..)
+                    .for_each(|value| _ = fingerprints.remove(unsafe { &*value.0 }));
+            }
+
+            tokio::time::sleep(Duration::from_millis(Self::PRUNE_INTERVAL));
+        }
+    }
 }
 
 pub struct EncryptionWindow {
