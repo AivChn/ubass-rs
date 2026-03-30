a632f6408f1a7d9745acadf3ac46f5ae40be1e66
aiv <amishavhere@gmail.com>
2026-03-30 15:44:25 +0300

    added Payload trait and removed the Headers constraint from the Fingerprint trait

--- src/packetizer/fingerprint.rs
+++ src/packetizer/fingerprint.rs
@@ -2,8 +2,18 @@ pub trait Headers {
     fn headers(&self) -> Vec<u8>;
 }
 
-pub trait Fingerprint: Headers {
+pub trait Payload: Headers {
+    fn payload(&mut self) -> &mut Vec<u8>;
+}
+
+pub trait Fingerprint {
     fn fingerprint(&self) -> [u8; 16];
+}
+
+impl Fingerprint for Vec<u8> {
+    fn fingerprint(&self) -> [u8; 16] {
+        xxhash_rust::xxh3::xxh3_128(self).to_be_bytes()
+    }
 }
 
 impl<T: Headers> Fingerprint for T {
