2e52d4dc4b1a1db27817fe090f0cf2e72674bac5
aiv <amishavhere@gmail.com>
2026-03-30 11:19:38 +0300

    implemented the god damn thing

--- src/packet_processor/inbound.rs
+++ src/packet_processor/inbound.rs
@@ -1,20 +1,7 @@
-#![allow(unused_variables)]
-
-use std::sync::OnceLock;
-
 use crate::{
     manager::types::EncryptionMonitor,
     packet_processor::{encryption, serialize::Serialize},
-    packetizer::{
-        fingerprint::Headers,
-        types::{
-            AckPacket, AppRejectErrorPacket, ControlType, DataPacket, ErrorType, HelloPacket,
-            HostControlType, Options, PacketType, ParityPacket, PlaybackControlType,
-            PlaybackStatusPacket, RetransmitPacket, SessionControlType,
-            SessionDoesNotExistErrorPacket, TrackRequestPacket, UnexpectedPacketErrorPacket,
-            Version,
-        },
-    },
+    packetizer::{fingerprint::Headers, types::*},
     prelude::*,
 };
 
@@ -22,7 +9,9 @@ use super::types::{InboundChannels, PacketWrapper, ReceivedPacket};
 
 use tokio::sync::mpsc::Sender;
 
-static ENCRYPTION_MONITOR: OnceLock<&EncryptionMonitor> = OnceLock::new();
+const SESSION_ID_OFFSET: usize = 8;
+const PACKET_TYPE_OFFSET: usize = 4;
+const SECONDARY_TYPE_OFFSET: usize = 5;
 
 pub async fn init(
     InboundChannels {
@@ -31,116 +20,193 @@ pub async fn init(
     }: InboundChannels,
     encryption_monitor: &'static EncryptionMonitor<'_>,
 ) -> ErrResult {
-    ENCRYPTION_MONITOR.set(encryption_monitor);
     loop {
         let mut buffer = Vec::with_capacity(16);
         let received = t_receiver.recv_many(&mut buffer, 16).await;
         if received == 0 {
             return Err(ChannelError::ChannelClosed(Inbound).into());
         }
-        handle_messages(buffer.into(), p_sender.clone());
+        handle_messages(buffer.into(), p_sender.clone(), encryption_monitor);
     }
-}
-
-fn get_encryption_monitor() -> &'static EncryptionMonitor<'static> {
-    ENCRYPTION_MONITOR.get().expect(
-        "Invariant broken while accessing ENCRYPTION_MONITOR: \
-    ENCRYPTION_MONITOR not initialized",
-    )
 }
 
 async fn handle_messages(
     buffer: Box<[Result<ReceivedPacket>]>,
     sender: Sender<Result<PacketWrapper>>,
+    encryption_monitor: &'static EncryptionMonitor<'_>,
 ) {
     for packet in buffer {
-        // TODO: Erorr handling
-        let packet = packet.expect("Error handling");
+        let packet = match packet {
+            Ok(packet) => packet,
+            Err(err) => {
+                if !err.is_recoverable() {
+                    tokio::spawn(send_up(Err(err), sender.clone()));
+                    continue;
+                } else {
+                    unreachable!("No pacth currently leads to a recoverable error at this level");
+                }
+            }
+        };
+
+        tokio::spawn(handle_packet(packet, sender.clone(), encryption_monitor));
     }
 }
 
-async fn handle_packet(packet: ReceivedPacket, sender: Sender<Result<PacketWrapper>>) {
-    if packet.data.len() < 2 {
-        return;
-    }
-
-    let Some(version) = Version::deserialize(&packet.data) else {
+async fn handle_packet(
+    mut packet: ReceivedPacket,
+    sender: Sender<Result<PacketWrapper>>,
+    encryption_monitor: &'static EncryptionMonitor<'_>,
+) {
+    let Ok(version) = Version::deserialize(&packet.data) else {
         return;
     };
 
+    if version.is_zero()
+        && let Ok(packet) = IncompatibleVersion::deserialize(&packet.data)
+    {
+        tokio::spawn(send_up(
+            Ok(PacketWrapper::IncompatibleVersion(Box::new(packet))),
+            sender.clone(),
+        ));
+        return;
+    }
+
     if !version.is_compatible() {
-        todo!("VersionIncompatible Error");
+        tokio::spawn(send_up(
+            Err(PacketProcessingError::IncompatibleVersion(version).into()),
+            sender.clone(),
+        ));
     }
 
     // opts is 16 bits, but isnt necessary to check right now
-    let Some((_, packet_type)) = <(u16, PacketType)>::deserialize(&packet.data[2..]) else {
+    let Ok(packet_type) = <PacketType>::deserialize(&packet.data[PACKET_TYPE_OFFSET..]) else {
         return;
     };
 
-    let Some(packet) = deserialize(packet_type, &packet.data) else {
-        todo!("IncoherentPacket Error");
+    let Ok(packet) = deserialize_and_decrypt(packet_type, &mut packet.data, encryption_monitor)
+    else {
+        return;
     };
+
+    send_up(Ok(packet), sender.clone()).await;
 }
 
-fn deserialize(packet_type: PacketType, data: &[u8]) -> Option<PacketWrapper> {
+fn handle_incompatible_version_packet(
+    data: &mut Vec<u8>,
+) -> core::result::Result<IncompatibleVersion, ()> {
+    IncompatibleVersion::deserialize(data)
+}
+
+fn deserialize_and_decrypt(
+    packet_type: PacketType,
+    data: &mut Vec<u8>,
+    encryption_monitor: &'static EncryptionMonitor<'_>,
+) -> core::result::Result<PacketWrapper, ()> {
     match packet_type {
         // Single types
-        PacketType::Data => Some(PacketWrapper::DataPacket(DataPacket::deserialize(&data)?)),
-        PacketType::Parity => Some(PacketWrapper::ParityPacket(ParityPacket::deserialize(
-            &data,
-        )?)),
-        PacketType::Ack => Some(PacketWrapper::AckPacket(AckPacket::deserialize(&data)?)),
+        PacketType::Data => {
+            let mut packet = Box::new(DataPacket::deserialize(data)?);
+            if !encryption::decrypt(
+                encryption::Encryptable::Data(&mut packet),
+                encryption_monitor,
+            ) {
+                Err(())
+            } else {
+                Ok(PacketWrapper::DataPacket(packet))
+            }
+        }
+        PacketType::Parity => {
+            let mut packet = Box::new(ParityPacket::deserialize(data)?);
+            if !encryption::decrypt(
+                encryption::Encryptable::Parity(&mut packet),
+                encryption_monitor,
+            ) {
+                Err(())
+            } else {
+                Ok(PacketWrapper::ParityPacket(packet))
+            }
+        }
+        PacketType::Ack => {
+            authenticate(data, encryption_monitor)?;
+            let mut packet = PacketWrapper::AckPacket(Box::new(AckPacket::deserialize(data)?));
+
+            Ok(packet)
+        }
 
         // Subtypes
         PacketType::Host | PacketType::Session | PacketType::Playback => {
-            deserialize_control_packet(data)
+            deserialize_and_auth_control_packet(data, encryption_monitor)
         }
-        PacketType::Error => deserialize_error_packet(data),
+        PacketType::Error => deserialize_error_packet(data, encryption_monitor),
 
         // Not yet
         PacketType::Metadata => unimplemented!(),
     }
 }
 
-fn deserialize_control_packet(data: &[u8]) -> Option<PacketWrapper> {
-    Some(match ControlType::deserialize(&data[5..])? {
-        ControlType::Host(host_control_type) => match host_control_type {
-            HostControlType::Hello => PacketWrapper::HelloPacket(HelloPacket::deserialize(data)?),
-        },
-        ControlType::Session(session_control_type) => match session_control_type {
-            SessionControlType::Retransmit => {
-                PacketWrapper::RetransmitPacket(RetransmitPacket::deserialize(data)?)
+fn deserialize_and_auth_control_packet(
+    data: &mut Vec<u8>,
+    encryption_monitor: &'static EncryptionMonitor<'_>,
+) -> core::result::Result<PacketWrapper, ()> {
+    authenticate(data, encryption_monitor)?;
+
+    Ok(
+        match ControlType::deserialize(&data[SECONDARY_TYPE_OFFSET..])? {
+            // host
+            ControlType::Host(HostControlType::Hello) => {
+                PacketWrapper::HelloPacket(Box::new(HelloPacket::deserialize(data)?))
             }
-            SessionControlType::TrackRequest => {
-                PacketWrapper::TrackRequestPacket(TrackRequestPacket::deserialize(data)?)
+            //session
+            ControlType::Session(SessionControlType::Retransmit) => {
+                PacketWrapper::RetransmitPacket(Box::new(RetransmitPacket::deserialize(data)?))
             }
-            SessionControlType::MetadataRequest => unimplemented!(),
+            ControlType::Session(SessionControlType::TrackRequest) => {
+                PacketWrapper::TrackRequestPacket(Box::new(TrackRequestPacket::deserialize(data)?))
+            }
+            ControlType::Session(SessionControlType::MetadataRequest) => unimplemented!(),
+            //playback
+            ControlType::Playback(_) => PacketWrapper::PlaybackStatusPacket(Box::new(
+                PlaybackStatusPacket::deserialize(data)?,
+            )),
         },
-        ControlType::Playback(_) => {
-            PacketWrapper::PlaybackStatusPacket(PlaybackStatusPacket::deserialize(data)?)
-        }
-    })
+    )
 }
 
-fn deserialize_error_packet(data: &[u8]) -> Option<PacketWrapper> {
-    Some(match ErrorType::deserialize(&data[5..])? {
-        ErrorType::AppReject => {
-            PacketWrapper::AppRejectErrorPacket(AppRejectErrorPacket::deserialize(data)?)
-        }
-        ErrorType::UnexpectedPacket | ErrorType::IncomprehensiblePacket => {
-            PacketWrapper::UnexpectedPacketErrorPacket(UnexpectedPacketErrorPacket::deserialize(
-                data,
-            )?)
-        }
-        ErrorType::SessionDoesNotExist => PacketWrapper::SessionDoesNotExistErrorPacket(
-            SessionDoesNotExistErrorPacket::deserialize(data)?,
-        ),
-    })
+fn deserialize_error_packet(
+    data: &mut Vec<u8>,
+    encryption_monitor: &'static EncryptionMonitor<'_>,
+) -> core::result::Result<PacketWrapper, ()> {
+    authenticate(data, encryption_monitor)?;
+
+    Ok(
+        match ErrorType::deserialize(&data[SECONDARY_TYPE_OFFSET..])? {
+            ErrorType::AppReject => PacketWrapper::AppRejectErrorPacket(Box::new(
+                AppRejectErrorPacket::deserialize(data)?,
+            )),
+            ErrorType::UnexpectedPacket | ErrorType::IncomprehensiblePacket => {
+                PacketWrapper::UnexpectedPacketErrorPacket(Box::new(
+                    UnexpectedPacketErrorPacket::deserialize(data)?,
+                ))
+            }
+            ErrorType::SessionDoesNotExist => PacketWrapper::SessionDoesNotExistErrorPacket(
+                Box::new(SessionDoesNotExistErrorPacket::deserialize(data)?),
+            ),
+        },
+    )
 }
 
-fn decrypt_packet(packet: encryption::Encryptable) -> bool {
-    match packet {
-        encryption::Encryptable::Data(data_packet) => todo!(),
-        encryption::Encryptable::Parity(parity_packet) => todo!(),
+fn authenticate(
+    packet: &mut Vec<u8>,
+    encryption_monitor: &'static EncryptionMonitor<'_>,
+) -> core::result::Result<(), ()> {
+    let session_id = SessionId::deserialize(&packet[SESSION_ID_OFFSET..])?;
+    if !encryption::authenticate(packet, session_id, encryption_monitor) {
+        Err(())
+    } else {
+        Ok(())
     }
+}
+
+async fn send_up(message: Result<PacketWrapper>, sender: Sender<Result<PacketWrapper>>) {
+    sender.send(message).await;
 }
