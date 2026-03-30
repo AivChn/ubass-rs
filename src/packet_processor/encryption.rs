794e8e5c774b028e9eeb3f6e72f1ad150266e109
aiv <amishavhere@gmail.com>
2026-03-30 15:47:58 +0300

    replaced Encryptable enum with Encryptable: Headers + Payload trait and updated decrypt function to return EmptyResult instead of a bool

--- src/packet_processor/encryption.rs
+++ src/packet_processor/encryption.rs
@@ -1,16 +1,20 @@
 use crate::manager::types::EncryptionMonitor;
-use crate::packetizer::fingerprint::Headers;
-use crate::packetizer::types::{DataPacket, ParityPacket, SessionId};
+use crate::packetizer::fingerprint::{Headers, Payload};
+use crate::packetizer::types::{
+    AppRejectErrorPacket, DataPacket, ParityPacket, RetransmitPacket, SessionId, TrackRequestPacket,
+};
 use crate::prelude::*;
 
 use aes_gcm_siv::{AeadInPlace, Aes256GcmSiv, Nonce};
 
-pub enum Encryptable<'a> {
-    Data(&'a mut DataPacket),
-    Parity(&'a mut ParityPacket),
-}
+trait Encryptable: Payload + Headers {}
 
-fn get_nonce(session_id: SessionId, counter: [u8; 8]) -> [u8; 12] {
+impl Encryptable for DataPacket {}
+impl Encryptable for ParityPacket {}
+impl Encryptable for TrackRequestPacket {}
+impl Encryptable for AppRejectErrorPacket {}
+
+fn get_nonce(session_id: &SessionId, counter: [u8; 8]) -> [u8; 12] {
     let mut result = [0u8; 12];
     let session_part = &session_id.0.to_be_bytes()[..4];
     result[..4].copy_from_slice(session_part);
@@ -18,29 +22,29 @@ fn get_nonce(session_id: SessionId, counter: [u8; 8]) -> [u8; 12] {
     result
 }
 
-pub fn encrypt(packet: &mut Encryptable, monitor: &EncryptionMonitor) {
-    let (aad, payload, session_id) = match packet {
-        Encryptable::Data(packet) => (packet.headers(), &mut packet.payload, packet.session_id),
-        Encryptable::Parity(packet) => (packet.headers(), &mut packet.payload, packet.session_id),
-    };
+#[allow(private_bounds)]
+pub fn encrypt(packet: &mut impl Encryptable, session_id: &SessionId, monitor: &EncryptionMonitor) {
+    let (aad, payload) = (packet.headers(), packet.payload());
 
     let (cipher, counter) = monitor.get(&session_id);
-    let nonce = Nonce::from(get_nonce(session_id, counter));
+    let nonce = Nonce::from(get_nonce(&session_id, counter));
 
     cipher.encrypt_in_place(&nonce, &aad, payload);
 
     payload.extend(counter);
 }
 
-pub fn decrypt(packet: Encryptable, monitor: &EncryptionMonitor) -> bool {
-    let (aad, payload, session_id) = match packet {
-        Encryptable::Data(packet) => (packet.headers(), &mut packet.payload, packet.session_id),
-        Encryptable::Parity(packet) => (packet.headers(), &mut packet.payload, packet.session_id),
-    };
+#[allow(private_bounds)]
+pub fn decrypt(
+    packet: &mut impl Encryptable,
+    session_id: &SessionId,
+    monitor: &EncryptionMonitor,
+) -> EmptyResult {
+    let (aad, payload) = (packet.headers(), packet.payload());
 
     // len < minimum payload + tag + nonce counter
     if payload.len() < 1 + 16 + 8 {
-        return false;
+        return Err(());
     }
 
     let cipher = monitor.get_cipher(&session_id);
@@ -48,13 +52,15 @@ pub fn decrypt(packet: Encryptable, monitor: &EncryptionMonitor) -> bool {
         .try_into()
         .expect("length is guaranteed");
     payload.truncate(payload.len() - 8);
-    let nonce = Nonce::from(get_nonce(session_id, counter));
+    let nonce = Nonce::from(get_nonce(&session_id, counter));
 
-    cipher.decrypt_in_place(&nonce, &aad, payload).is_ok()
+    cipher
+        .decrypt_in_place(&nonce, &aad, payload)
+        .map_err(|_| ())
 }
 
-pub fn tag(packet: &mut Vec<u8>, session_id: SessionId, monitor: &EncryptionMonitor) {
-    let (cipher, counter) = monitor.get(&session_id);
+pub fn tag(packet: &mut Vec<u8>, session_id: &SessionId, monitor: &EncryptionMonitor) {
+    let (cipher, counter) = monitor.get(session_id);
     let nonce = Nonce::from(get_nonce(session_id, counter));
 
     let mut tag = vec![];
@@ -66,7 +72,7 @@ pub fn tag(packet: &mut Vec<u8>, session_id: SessionId, monitor: &EncryptionMoni
 
 pub fn authenticate(
     packet: &mut Vec<u8>,
-    session_id: SessionId,
+    session_id: &SessionId,
     monitor: &EncryptionMonitor,
 ) -> bool {
     let cipher = monitor.get_cipher(&session_id);
