7259ee17a2181d261dd08a0b36a12bc7f13cf901
aiv <amishavhere@gmail.com>
2026-04-05 15:12:31 +0300

    removed state structs and moved to state.rs

--- src/manager/types.rs
+++ src/manager/types.rs
@@ -9,339 +9,53 @@ use std::{
 };
 
 use aes_gcm_siv::Aes256GcmSiv;
-use tokio::sync::{
-    Mutex, RwLock,
-    mpsc::{Receiver, Sender},
+use tokio::{
+    sync::{
+        Mutex, RwLock,
+        mpsc::{Receiver, Sender},
+    },
+    time::Instant,
 };
 
 use crate::{
-    manager::packets::{PacketFingerprint, PacketWrapper, SessionId, Timestamp},
+    manager::packets::{PacketFingerprint, PacketWrapper, SessionId},
+    packet_processor::serialize::Serialize,
     prelude::*,
 };
-
-const PACKET_DISCARD_TIME_MS: u64 = 7 * 1000;
 
 type OutboundReceiver = Receiver<Result<ManagerMessage>>;
 type OutboundSender = Sender<PacketProcessingMessage>;
 
-pub struct PendingAck {
-    packet: PacketWrapper,
-    timestamp: Timestamp,
-    retries: u8,
-}
+#[derive(Serialize, Clone, Copy, PartialEq, Debug)]
+#[repr(transparent)]
+pub struct Timestamp(u64);
 
-impl PendingAck {
-    const MAX_RETRIES: u8 = 5;
-
-    pub fn new(packet: PacketWrapper, timestamp: Timestamp) -> Self {
-        Self {
-            packet,
-            timestamp,
-            retries: 0,
-        }
-    }
-
-    pub fn packet(&self) -> &PacketWrapper {
-        &self.packet
-    }
-
-    pub fn retried(&mut self) {
-        self.timestamp = Timestamp::now();
-        self.retries += 1;
-    }
-
-    pub fn is_expired(&self) -> bool {
-        self.timestamp.been_longer_than(PACKET_DISCARD_TIME_MS) && self.retries >= Self::MAX_RETRIES
-    }
-}
-
-type PendingAckTable = HashMap<PacketFingerprint, PendingAck>;
-
-pub struct PendingAckMonitor<'a> {
-    table: &'a RwLock<PendingAckTable>,
-}
-
-impl<'a> PendingAckMonitor<'a> {
-    pub fn new(table: &'a RwLock<PendingAckTable>) -> Self {
-        Self { table }
-    }
-
-    pub async fn add(
-        &self,
-        fingerprint: PacketFingerprint,
-        pending_ack: (PacketWrapper, Timestamp),
-    ) {
-        let mut table = self.table.write().await;
-        table.insert(fingerprint, PendingAck::new(pending_ack.0, pending_ack.1));
-    }
-}
-
-#[derive(Eq)]
-struct FingerprintPtr(*const PacketFingerprint);
-
-unsafe impl Send for FingerprintPtr {}
-unsafe impl Sync for FingerprintPtr {}
-
-impl FingerprintPtr {
-    fn from_box(value: &PacketFingerprint) -> Self {
-        Self(std::ptr::from_ref(value))
-    }
-}
-
-impl PartialEq for FingerprintPtr {
-    fn eq(&self, other: &Self) -> bool {
-        unsafe { (*self.0) == (*other.0) }
-    }
-}
-
-impl Hash for FingerprintPtr {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        unsafe {
-            (*self.0).hash(state);
-        }
-    }
-}
-
-pub struct FingerprintMonitor<'a> {
-    table: &'a RwLock<HashMap<SessionId, Arc<FingerprintWindow>>>,
-}
-
-impl<'a> FingerprintMonitor<'a> {
-    pub fn new(table: &'a RwLock<HashMap<SessionId, Arc<FingerprintWindow>>>) -> Self {
-        Self { table }
-    }
-
-    pub async fn add(&self, session_id: SessionId) {
-        let mut table = self.table.write().await;
-        table.insert(session_id, Arc::default());
-    }
-
-    /// returns an Arc to the window for this session
+impl Timestamp {
+    /// returns the current time since `PROTOCOL_EPOCH`
     ///
     /// # Panics
-    /// This function panics if the session is not yet initialized - an invairant
-    pub async fn get(&self, session_id: &SessionId) -> Arc<FingerprintWindow> {
-        let table = self.table.read().await;
-        let Some(window) = table.get(session_id) else {
-            panic!(
-                "Invairant broken while trying to get a `FingerprintWindow`:\
-            {session_id} is not a valid session"
-            );
-        };
-
-        window.clone()
-    }
-}
-
-pub struct FingerprintWindow {
-    fingerprints: RwLock<HashSet<Box<PacketFingerprint>>>,
-    queue: Mutex<VecDeque<(Timestamp, FingerprintPtr)>>,
-    canceled: AtomicBool,
-}
-
-impl Default for FingerprintWindow {
-    fn default() -> Self {
-        Self {
-            fingerprints: RwLock::new(HashSet::new()),
-            queue: Mutex::new(VecDeque::new()),
-            canceled: AtomicBool::new(false),
-        }
-    }
-}
-
-impl FingerprintWindow {
-    const PRUNE_INTERVAL: u64 = PACKET_DISCARD_TIME_MS;
-    const BUFFERING_TIME: u64 = 2 * 1000;
-
-    pub fn init(self: Arc<Self>) {
-        tokio::spawn(self.prune());
+    /// This function panics if `PROTOCOL_EPOCH` is not yet initialized - an invariant
+    pub fn now() -> Self {
+        #[allow(clippy::cast_possible_truncation)]
+        Self(
+            Instant::now()
+                .duration_since(*PROTOCOL_EPOCH.get().expect(
+                    "Invariant broken while constructing `Timestamp`: \
+        `PROTOCOL_EPOCH` is not initialized",
+                ))
+                .as_millis() as u64,
+        )
     }
 
-    #[must_use]
-    pub async fn contains(&self, fingerprint: &PacketFingerprint) -> bool {
-        let fingerprints = self.fingerprints.read().await;
-        fingerprints.contains(fingerprint)
+    pub fn been_longer_than(&self, millis: u64) -> bool {
+        Self::now().0 - self.0 > millis
     }
 
-    pub async fn add(&self, fingerprint: Box<PacketFingerprint>) -> bool {
-        let ptr = {
-            let mut fingerprints = self.fingerprints.write().await;
-            let ptr = FingerprintPtr::from_box(&fingerprint);
-            if fingerprints.insert(fingerprint) {
-                Some(ptr)
-            } else {
-                None
-            }
-        };
-
-        let Some(ptr) = ptr else {
-            return false;
-        };
-
-        {
-            let mut queue = self.queue.lock().await;
-            queue.push_back((Timestamp::now(), ptr));
-        }
-
-        true
+    pub fn none() -> Self {
+        Timestamp(0)
     }
 
-    pub async fn prune(self: Arc<Self>) {
-        let mut expired = Vec::with_capacity(256);
-        while !self.canceled.load(Ordering::Relaxed) {
-            let top_timestamp;
-            {
-                let mut queue = self.queue.lock().await;
-                while queue
-                    .front()
-                    .is_some_and(|(ts, _)| ts.been_longer_than(Self::PRUNE_INTERVAL))
-                {
-                    if let Some((_, value)) = queue.pop_front() {
-                        expired.push(value);
-                    }
-                }
-                top_timestamp = {
-                    if let Some(top) = queue.front() {
-                        top.0.get()
-                    } else {
-                        Self::PRUNE_INTERVAL - Self::BUFFERING_TIME
-                    }
-                }
-            }
-
-            {
-                let mut fingerprints = self.fingerprints.write().await;
-                expired
-                    .drain(..)
-                    .for_each(|value| _ = fingerprints.remove(unsafe { &*value.0 }));
-            }
-
-            tokio::time::sleep(Duration::from_millis(top_timestamp + Self::BUFFERING_TIME)).await;
-        }
-    }
-}
-
-pub struct EncryptionWindow {
-    cipher: Aes256GcmSiv,
-    nonce: AtomicU64,
-}
-
-impl EncryptionWindow {
-    pub fn new(cipher: Aes256GcmSiv) -> Self {
-        Self {
-            cipher,
-            nonce: AtomicU64::new(0),
-        }
-    }
-
-    pub fn get(&self) -> (&Aes256GcmSiv, [u8; 8]) {
-        let x = self
-            .nonce
-            .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-        (&self.cipher, x.to_be_bytes())
-    }
-
-    pub fn get_cipher(&self) -> &Aes256GcmSiv {
-        &self.cipher
-    }
-}
-
-type EncryptionTable = HashMap<SessionId, EncryptionWindow>;
-
-pub struct EncryptionMonitor<'a> {
-    table: &'a EncryptionTable,
-}
-
-impl<'a> EncryptionMonitor<'a> {
-    fn new(table: &'a EncryptionTable) -> Self {
-        Self { table }
-    }
-
-    /// returns the key and nonce counter, increasing it in the process, for a specific session
-    ///
-    /// # Panics
-    /// This function panics if the key is not yet created, which should be impossible
-    pub fn get(&self, session_id: &SessionId) -> (&Aes256GcmSiv, [u8; 8]) {
-        self.table
-            .get(session_id)
-            .unwrap_or_else(|| {
-                panic!(
-                    "Invairant broken while accessing session table: \
-        session ({session_id}) does not have a key but is being accessed for encryption",
-                )
-            })
-            .get()
-    }
-
-    /// returns the key without increasing the counter
-    ///
-    /// # Panics
-    /// This function panics if the key is not yet created, which should be impossible
-    pub fn get_cipher(&self, session_id: &SessionId) -> &Aes256GcmSiv {
-        self.table
-            .get(session_id)
-            .unwrap_or_else(|| {
-                panic!(
-                    "Invairant broken while accessing session table: \
-        session ({session_id}) does not have a key but is being accessed for encryption",
-                )
-            })
-            .get_cipher()
-    }
-}
-
-struct SessionTable {
-    encryption: EncryptionTable,
-}
-
-impl SessionTable {
-    pub fn get_encryption_monitor(&self) -> EncryptionMonitor<'_> {
-        EncryptionMonitor::new(&self.encryption)
-    }
-}
-
-struct TmpAddressSessionMapper {
-    factor: u64,
-}
-
-/// temporary monitor to give transport layer access to the address-session bindings
-pub struct AddressSessionMonitor {
-    table: Arc<TmpAddressSessionMapper>,
-}
-
-impl AddressSessionMonitor {
-    /// deterministcally returns the session id based on a mapping
-    /// from the address. Even if the session doesn't exist.
-    pub fn get_session_id(&self, addr: (u8, u8, u8, u8, u16)) -> SessionId {
-        let full: u64 = (addr.0 as u64) << 40
-            | (addr.1 as u64) << 32
-            | (addr.2 as u64) << 24
-            | (addr.3 as u64) << 16
-            | addr.4 as u64;
-        SessionId(full * self.table.factor)
-    }
-
-    /// deterministcally returns the address based on a mapping
-    /// from the session id. Even if the session doesn't exist.
-    pub fn get_addr(&self, session_id: SessionId) -> String {
-        #![allow(clippy::many_single_char_names)]
-        let unfactored = session_id.0 / self.table.factor;
-        let a = ((unfactored >> 40) & 0xFF) as u8;
-        let b = ((unfactored >> 32) & 0xFF) as u8;
-        let c = ((unfactored >> 24) & 0xFF) as u8;
-        let d = (unfactored >> 16) & 0xFF;
-        let p = (unfactored & 0xFFFF) as u16;
-
-        format!("{a}.{b}.{c}.{d}:{p}")
-    }
-}
-
-impl Default for AddressSessionMonitor {
-    fn default() -> Self {
-        Self {
-            table: Arc::new(TmpAddressSessionMapper {
-                factor: 333_333_333,
-            }),
-        }
+    pub fn get(&self) -> u64 {
+        self.0
     }
 }
