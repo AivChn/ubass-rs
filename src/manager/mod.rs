ab3db417f3468c25e1898c06e784e37ed0679f62
aiv <amishavhere@gmail.com>
2026-04-05 15:10:55 +0300

    added some public imports from the state module

--- src/manager/mod.rs
+++ src/manager/mod.rs
@@ -2,12 +2,19 @@ mod inbound;
 mod key_exchange;
 mod outbound;
 pub mod packets;
+mod state;
 pub mod types;
 
 use crate::prelude::*;
 
 use tokio::time::Instant;
 
+pub use state::{AppId, EncryptionMonitor, FingerprintMonitor, PendingAckMonitor};
+
 pub fn init() {
     PROTOCOL_EPOCH.get_or_init(Instant::now);
+}
+
+fn initiate_handshake() -> Result<i32> {
+    todo!()
 }
