46b537955ffd905834c2ab32d25f4e4da25c4d1f
aiv <amishavhere@gmail.com>
2026-04-05 15:14:35 +0300

    aliased HashMap<K, V> to FxHashMap<K, V>

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,8 +2,11 @@ use std::sync::OnceLock;
 use tokio::time::Instant;
 
 pub use crate::error::*;
-pub use crate::manager::packets;
+pub use crate::manager::{packets, types::Timestamp};
+pub use crate::packet_processor::serialize::Serialize;
 pub use crate::utils::*;
 pub use ubass_macros::*;
+
+pub type HashMap<K, V> = rustc_hash::FxHashMap<K, V>;
 
 pub static PROTOCOL_EPOCH: OnceLock<Instant> = OnceLock::new();
