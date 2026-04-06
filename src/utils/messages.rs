848890c5a5dd482661c08d64b2534f1095584f8f
aiv <amishavhere@gmail.com>
2026-04-06 12:53:09 +0300

    added messages to the api layer and a OneShot<T: Send> wrapper type to receive one shot responses from the api layer

--- src/utils/messages.rs
+++ src/utils/messages.rs
@@ -1,9 +1,39 @@
+use tokio::sync::oneshot;
+
+use crate::manager::AppId;
 use crate::prelude::*;
 use crate::{
     manager::packets::{BatchID, PacketWrapper, SessionId},
     packet_processor::{fec::RecoverdPacket, types::ProcessedPacket},
     transport::types::ReceivedPacket,
 };
+
+pub struct OneShot<T: Send> {
+    data: T,
+    reply: oneshot::Sender<AppResponse>,
+}
+
+impl<T: Send> OneShot<T> {
+    pub fn new(value: T) -> (Self, oneshot::Receiver<AppResponse>) {
+        let (sender, receiver) = oneshot::channel();
+        (
+            Self {
+                data: value,
+                reply: sender,
+            },
+            receiver,
+        )
+    }
+}
+
+pub enum AppRequest {
+    HelloAppId(OneShot<AppId>),
+}
+
+pub enum AppResponse {
+    AppApproved(String),
+    AppRejected(String),
+}
 
 pub enum ManagerMessage {
     Recovered(Vec<RecoverdPacket>),
