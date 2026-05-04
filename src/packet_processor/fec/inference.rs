18e22b8230e0bf0ddac95c26c2c0e07d1ad968ea
aiv <amishavhere@gmail.com>
2026-05-05 00:17:24 +0300

    added placeholder values for FEC batches

--- src/packet_processor/fec/inference.rs
+++ src/packet_processor/fec/inference.rs
@@ -1,3 +1,22 @@
-use crate::manager::packets::{BatchID, FECInfo};
+use crate::manager::{
+    self,
+    packets::{BatchID, FECInfo},
+};
 
-pub const TEMP_FEC: FECInfo = FECInfo::const_new(32, 0, 1);
+pub const MAGIC_BATCH_SIZE: usize = manager::state::PACKET_COUNT_PER_BATCH;
+
+pub struct BatchInfo {
+    batch_size: u8,
+    recovery_count: u8,
+}
+
+#[must_use]
+pub fn create_batch(packets_left: usize) -> BatchInfo {
+    // this picks the minimum between any number and 28, it will be within the u8 range
+    #[allow(clippy::cast_possible_truncation)]
+    let batch_size = packets_left.min(MAGIC_BATCH_SIZE) as u8;
+    BatchInfo {
+        batch_size,
+        recovery_count: 1,
+    }
+}
